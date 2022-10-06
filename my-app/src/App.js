6182ba6958274576d4b778452f8cb17b51c2a1b6
Andrew <andrew.sf.liao@gmail.com>
2022-10-05 21:48:52 -0500

    fetch data

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,8 @@
+import React, { useState,  useEffect  } from 'react';
 import './App.css';
 import Banner from "./components/Banner";
 import CourseList from "./components/CourseList";
+
 
 const schedule = {
   "title": "CS Courses for 2018-2019",
@@ -32,13 +34,47 @@ const schedule = {
   }
 };
 
-function App() {
+const terms = { F: 'Fall', W: 'Winter', S: 'Spring'};
+
+const getCourseTerm = course => (
+  terms[course.id.charAt(0)]
+);
+
+const getCourseNumber = course => (
+  course.id.slice(1, 4)
+);
+
+const Course = ({ course }) => (
+  <div className="card m-1 p-2">
+    <div className="card-body">
+      <div className="card-title">{ getCourseTerm(course) } CS { getCourseNumber(course) }</div>
+      <div className="card-text">{ course.title }</div>
+    </div>
+  </div>
+);
+
+const App = () => {
+  const [schedule, setSchedule] = useState();
+  const url = 'https://courses.cs.northwestern.edu/394/data/cs-courses.php';
+
+  useEffect(() => {
+    const fetchSchedule = async () => {
+      const response = await fetch(url);
+      if (!response.ok) throw response;
+      const json = await response.json();
+      setSchedule(json);
+    }
+    fetchSchedule();
+  }, []);
+
+  if (!schedule) return <h1>Loading schedule...</h1>;
+
   return (
     <div className="App">
-      <Banner title={schedule.title} />
-      <CourseList courses={schedule.courses} />
+      <Banner title={ schedule.title } />
+      <CourseList courses={ schedule.courses } />
     </div>
   );
-}
+};
 
 export default App;
